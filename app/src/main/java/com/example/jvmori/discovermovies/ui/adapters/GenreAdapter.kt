c00b3fa3cbcddf6f2ca1a20e34904188004529b6
Mori <iwona.moryc92@gmail.com>
2019-02-23 19:04:21 +0100

    Navigate to movie list

--- app/src/main/java/com/example/jvmori/discovermovies/ui/adapters/GenreAdapter.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/ui/adapters/GenreAdapter.kt
@@ -3,9 +3,11 @@ package com.example.jvmori.discovermovies.ui.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jvmori.discovermovies.R
 import com.example.jvmori.discovermovies.data.network.response.Genre
+import com.example.jvmori.discovermovies.ui.discover.DiscoverFragmentDirections
 import com.example.jvmori.discovermovies.util.RandomColor
 import kotlinx.android.synthetic.main.genre_item.view.*
 
@@ -24,10 +26,15 @@ class GenreAdapter(
         holder.itemView.itemName.text = allGenres[position].name
         holder.itemView.cardView2.setCardBackgroundColor(RandomColor.generateColor())
         holder.itemView.setOnClickListener {
-
+           navigateToMovieList(it, position)
         }
     }
 
     class GenreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
+    private fun navigateToMovieList(view: View, position: Int){
+        val id = allGenres[position].id
+        val action = DiscoverFragmentDirections.specifyGenreId().setGenre(id)
+        Navigation.findNavController(view).navigate(action)
+    }
 }
