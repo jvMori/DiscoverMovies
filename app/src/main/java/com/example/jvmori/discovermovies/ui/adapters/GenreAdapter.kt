ff7becd7c364f8d12747149a331910a58b27c10f
Mori <iwona.moryc92@gmail.com>
2019-02-23 17:22:20 +0100

    Implementing genres recycler view's adapter

---
+++ app/src/main/java/com/example/jvmori/discovermovies/ui/adapters/GenreAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.jvmori.discovermovies.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.jvmori.discovermovies.R
+import com.example.jvmori.discovermovies.data.network.response.Genre
+import kotlinx.android.synthetic.main.genre_item.view.*
+
+class GenreAdapter(
+    private val allGenres: List<Genre> = mutableListOf()
+) : RecyclerView.Adapter<GenreAdapter.GenreViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenreViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.genre_item, parent, false)
+        return GenreViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = allGenres.size
+
+    override fun onBindViewHolder(holder: GenreViewHolder, position: Int) {
+        holder.itemView.itemName.text = allGenres[position].name
+    }
+
+    class GenreViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
