5bcfb3ae278134d5396150635b01c4d33a03370a
Mori <iwona.moryc92@gmail.com>
2019-03-01 15:26:05 +0100

    Movie data source factory

--- app/src/main/java/com/example/jvmori/discovermovies/ui/view/paging/MovieDataSourceFactory.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/ui/view/paging/MovieDataSourceFactory.kt
@@ -1,16 +1,21 @@
 package com.example.jvmori.discovermovies.ui.view.paging
 
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.DataSource
+import androidx.paging.PageKeyedDataSource
 import com.example.jvmori.discovermovies.data.network.response.MovieResult
 import com.example.jvmori.discovermovies.data.repository.MoviesRepository
 import com.example.jvmori.discovermovies.ui.view.movies.DiscoverQueryParam
 
 class MovieDataSourceFactory(
-    private val repository : MoviesRepository,
-    private val parameters : DiscoverQueryParam
+    private val repository: MoviesRepository,
+    private val parameters: DiscoverQueryParam
 ) : DataSource.Factory<Int, MovieResult>() {
 
+    private val dataSourceLiveData: MutableLiveData<PageKeyedDataSource<Int, MovieResult>> = MutableLiveData()
     override fun create(): DataSource<Int, MovieResult> {
-       return MoviesDataSource(repository, parameters)
+        val movieDataSource = MoviesDataSource(repository, parameters)
+        dataSourceLiveData.postValue(movieDataSource)
+        return movieDataSource
     }
 }
