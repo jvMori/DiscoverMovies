c15bbb88cc47663a7de6f648afe8645b840fc425
Mori <iwona.moryc92@gmail.com>
2019-03-04 16:10:49 +0100

    Update MovieDao.kt

--- app/src/main/java/com/example/jvmori/discovermovies/data/local/MovieDao.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/data/local/MovieDao.kt
@@ -2,6 +2,8 @@ package com.example.jvmori.discovermovies.data.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
 import com.example.jvmori.discovermovies.data.local.entity.DiscoverMovieResponse
 
 @Dao
@@ -9,4 +11,13 @@ interface MovieDao
 {
     @Insert
     fun insert(movies : DiscoverMovieResponse)
+
+    @Transaction
+    fun updateData(movies : DiscoverMovieResponse){
+        deleteUsers(movies.genreId, movies.page)
+        insert(movies)
+    }
+    
+    @Query("Delete from movies_discover where genreId like :genreId and page like :page")
+    fun deleteUsers(genreId : Int, page: Int)
 }
