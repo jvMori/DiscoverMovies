81403fa03b6572d8b07f94c21f92098e8d39aff8
Mori <iwona.moryc92@gmail.com>
2019-03-05 21:27:44 +0100

    setup bottom nav

--- app/src/main/java/com/example/jvmori/discovermovies/MainActivity.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.jvmori.discovermovies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val controller = Navigation.findNavController(this, R.id.my_nav_host_fragment)
+        NavigationUI.setupWithNavController(bottom_navigation, controller)
     }
 }
