088354bc54313ab6e2aeea3ad668d8c7bee86920
jvMori <iwona.moryc92@gmail.com>
2019-04-25 20:49:42 +0200

    Update SavedMovieDao.kt

--- app/src/main/java/com/example/jvmori/discovermovies/data/local/SavedMovieDao.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/data/local/SavedMovieDao.kt
@@ -1,11 +1,20 @@
 package com.example.jvmori.discovermovies.data.local
-
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.jvmori.discovermovies.data.local.entity.MovieResult
+import io.reactivex.Observable
 
 @Dao
 interface SavedMovieDao {
+
     @Insert
     fun insert(movie: MovieResult)
+
+    @Delete
+    fun delete(movie: MovieResult)
+
+    @Query("Select * from saved_movies")
+    fun getAllSaved() : Observable<List<MovieResult>>
 }
