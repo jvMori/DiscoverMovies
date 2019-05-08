54c017d6540b5ed4f1fa40711ed518631b1bcdbe
jvMori <iwona.moryc92@gmail.com>
2019-05-08 20:26:36 +0200

    automatic sliding with timerTask

--- app/src/main/java/com/example/jvmori/discovermovies/MainActivity.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.jvmori.discovermovies
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG = "DiscoverMovies"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,4 +20,5 @@ class MainActivity : AppCompatActivity() {
         val controller = Navigation.findNavController(this, R.id.my_nav_host_fragment)
         NavigationUI.setupWithNavController(bottom_navigation, controller)
     }
+
 }
--- app/src/main/java/com/example/jvmori/discovermovies/ui/view/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/ui/view/discover/DiscoverFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
+import com.example.jvmori.discovermovies.MainActivity
 import com.example.jvmori.discovermovies.R
 import com.example.jvmori.discovermovies.application.MoviesApplication
 import com.example.jvmori.discovermovies.data.local.entity.Genre
@@ -18,6 +20,7 @@ import com.example.jvmori.discovermovies.ui.adapters.SliderPagerAdapter
 import com.example.jvmori.discovermovies.ui.presenter.genres.GenresPresenterInterface
 import com.example.jvmori.discovermovies.ui.presenter.genres.GenresViewInterface
 import kotlinx.android.synthetic.main.fragment_discover.*
+import java.util.*
 import javax.inject.Inject
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,15 +32,17 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class DiscoverFragment : Fragment(), GenresViewInterface{
+class DiscoverFragment : Fragment(), GenresViewInterface {
 
     @Inject
-    lateinit var genresPresenter : GenresPresenterInterface
-    private var genresMap = mutableMapOf<Int,String>()
-
+    lateinit var genresPresenter: GenresPresenterInterface
+    private var genresMap = mutableMapOf<Int, String>()
     val movies = mutableListOf<MovieResult>()
+    private lateinit var contextActivity: Context
+
     override fun onAttach(context: Context) {
         (context.applicationContext as MoviesApplication).movieComponent.inject(this)
+        if (context is MainActivity) contextActivity = context
         super.onAttach(context)
     }
 
@@ -45,15 +50,27 @@ class DiscoverFragment : Fragment(), GenresViewInterface{
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        movies.add(0, MovieResult(0,false,"","/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
-            mutableListOf(28,12,878),"pl","pl","",1.4, "","","Avengers: Endgame",
-            false,23.4,123))
-        movies.add(1, MovieResult(0,false,"","/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
-            mutableListOf(28,12,878),"pl","pl","",1.4, "","","Avengers: Endgame",
-            false,23.4,123))
-        movies.add(2, MovieResult(0,false,"","/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
-            mutableListOf(28,12,878),"pl","pl","",1.4, "","","Avengers: Endgame",
-            false,23.4,123))
+        movies.add(
+            0, MovieResult(
+                0, false, "", "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+                mutableListOf(28, 12, 878), "pl", "pl", "", 1.4, "", "", "Avengers: Endgame",
+                false, 23.4, 123
+            )
+        )
+        movies.add(
+            1, MovieResult(
+                0, false, "", "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+                mutableListOf(28, 12, 878), "pl", "pl", "", 1.4, "", "", "Avengers: Endgame",
+                false, 23.4, 123
+            )
+        )
+        movies.add(
+            2, MovieResult(
+                0, false, "", "/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg",
+                mutableListOf(28, 12, 878), "pl", "pl", "", 1.4, "", "", "Avengers: Endgame",
+                false, 23.4, 123
+            )
+        )
         return inflater.inflate(R.layout.fragment_discover, container, false)
     }
 
@@ -61,6 +78,8 @@ class DiscoverFragment : Fragment(), GenresViewInterface{
         super.onViewCreated(view, savedInstanceState)
         genresPresenter.setView(this)
         genresPresenter.getGenres()
+        val timer = Timer()
+        timer.scheduleAtFixedRate(SliderTimer(contextActivity, slider_pager, movies),4000,6000)
     }
 
     override fun displayGenres(genreResponse: List<Genre>) {
@@ -71,6 +90,7 @@ class DiscoverFragment : Fragment(), GenresViewInterface{
         slider_pager.adapter = adapter
         worm_dots_indicator.setViewPager(slider_pager)
     }
+
     override fun showProgressBar() {
 
     }
@@ -81,5 +101,21 @@ class DiscoverFragment : Fragment(), GenresViewInterface{
 
     override fun displayError(s: String) {
 
+    }
+
+    class SliderTimer (
+        private var context: Context,
+        private var sliderPager: ViewPager,
+        private val movies: List<MovieResult>
+    ) : TimerTask() {
+        override fun run() {
+            (context as MainActivity).runOnUiThread {
+                if (sliderPager.currentItem < movies.size - 1) {
+                    sliderPager.setCurrentItem(sliderPager.currentItem + 1, true)
+                } else {
+                    sliderPager.setCurrentItem(0, true)
+                }
+            }
+        }
     }
 }
