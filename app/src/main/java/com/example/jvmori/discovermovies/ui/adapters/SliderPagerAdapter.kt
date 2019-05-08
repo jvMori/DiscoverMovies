0bcefa04a890c053b680eacdec3ebccda6b82c3a
jvMori <iwona.moryc92@gmail.com>
2019-05-08 21:26:47 +0200

    Update SliderPagerAdapter.kt

--- app/src/main/java/com/example/jvmori/discovermovies/ui/adapters/SliderPagerAdapter.kt
+++ app/src/main/java/com/example/jvmori/discovermovies/ui/adapters/SliderPagerAdapter.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.viewpager.widget.PagerAdapter
 import com.example.jvmori.discovermovies.R
 import com.example.jvmori.discovermovies.data.local.entity.MovieResult
+import com.example.jvmori.discovermovies.util.Const
+import com.example.jvmori.discovermovies.util.LoadImage
 import com.example.jvmori.discovermovies.util.SetupGenres
 import kotlinx.android.synthetic.main.slide_item.view.*
 
@@ -23,9 +25,12 @@ class SliderPagerAdapter(
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val slideLayout = inflater.inflate(R.layout.slide_item, null)
+        val movieItem = movies[position]
 
-        slideLayout.slideTitle.text = movies[position].title
-        SetupGenres.setup(movies[position].genreIds, slideLayout.slideGenre, genres, movies[position].genreIds.size -1)
+        slideLayout.slideTitle.text = movieItem.title
+        slideLayout.review.text = movieItem.voteAverage.toString()
+        LoadImage.loadImage(context, slideLayout.posterSlide, Const.base_backdrop_url + movieItem.backdropPath)
+        SetupGenres.setup(movieItem.genreIds, slideLayout.slideGenre, genres, movies[position].genreIds.size -1)
 
         container.addView(slideLayout)
         return slideLayout
